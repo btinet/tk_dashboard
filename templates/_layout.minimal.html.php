6c51a7567c03a8b8476237a97c3d9a14eaa9c687
Benjamin Wagner <bwagner@vapita.de>
2022-12-11 19:22:59 +0100

    update layout

--- templates/_layout.minimal.html.php
+++ templates/_layout.minimal.html.php
@@ -36,81 +36,13 @@ $this->layout('base.html',
     ]);
 ?>
 
-    <header id="header" class="py-2 mb-0 border-bottom sticky-top shadow-sm">
+
         <?php if ($this->section('header')): ?>
             <?=$this->section('header')?>
         <?php else: ?>
-            <div class="container-fluid">
-                <div class="row g-3">
-                    <div class="col-5 col-md-3">
-                        <div class="d-flex justify-content-start align-items-center h-100">
-                            <a class="btn btn-light  me-1 d-block d-md-none" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-                                <i class="fa fa-fw fa-navicon" style="font-size: 1.2em"></i>
-                            </a>
 
-                            <a class="btn btn-light  me-1 d-none d-md-block" href="<?=$response->generateUrlFromRoute('app_index')?>">
-                                <i class="fa fa-fw fa-home" style="font-size: 1.2em"></i>
-                            </a>
-                            <?php if(!$session->get('login')): ?>
-                                <a class="btn btn-light " data-bs-toggle="tooltip" data-bs-title="Anmelden" href="<?=$response->generateUrlFromRoute('authentication_login')?>"><i class="fa fa-fw fa-sign-in" style="font-size: 1.2em"></i></a>
-                            <?php else: ?>
-                                <div class="dropdown">
-                                    <button class="btn btn-light  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <i class="fa fa-fw fa-user" style="font-size: 1.2em"></i>
-                                    </button>
-                                    <ul class="dropdown-menu">
-                                        <?php if($session->UserHasPermission('show_profile')): ?>
-                                        <li>
-                                            <a class="dropdown-item d-flex justify-content-start align-items-center" href="<?=$response->generateUrlFromRoute('user_profile_index')?>">
-                                                <i class="fa fa-fw fa-user me-1"></i>
-                                                Profil
-                                            </a>
-                                        </li>
-                                        <?php endif; ?>
-                                        <?php if($session->UserHasPermission('show_user_exam')): ?>
-                                        <li>
-                                            <a class="dropdown-item d-flex justify-content-start align-items-center" href="#">
-                                                <i class="fa fa-fw fa-file-text me-1"></i>
-                                                Meine Themen
-                                            </a>
-                                        </li>
-                                        <?php endif; ?>
-                                        <?php if($session->UserHasPermission('show_dashboard')): ?>
-                                            <li><hr class="dropdown-divider"></li>
-                                            <li>
-                                                <a class="dropdown-item d-flex justify-content-start align-items-center" href="<?=$response->generateUrlFromRoute('admin_index')?>">
-                                                    <i class="fa fa-fw fa-dashboard me-1"></i>
-                                                    Administration
-                                                </a>
-                                            </li>
-                                        <?php endif;?>
-                                        <li><hr class="dropdown-divider"></li>
-                                        <li>
-                                            <a class="dropdown-item d-flex justify-content-start align-items-center" href="<?=$response->generateUrlFromRoute('authentication_logout')?>">
-                                                <i class="fa fa-fw fa-sign-out text-danger me-1"></i>
-                                                Abmelden
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                    <div class="col-7 col-md-9">
-                        <div class="d-flex align-items-center">
-                            <form class="w-100" role="search" method="post" action="<?=$response->generateUrlFromRoute('app_search')?>">
-                                <div class="input-group">
-                                    <input type="search" class="form-control" name="search" placeholder="Stichwort..." aria-label="Suche" required>
-                                    <input type="hidden" class="d-none" name="csrf_token" value="<?=$session->get('csrf_token')?>">
-                                    <button class="btn btn-primary" type="submit" role="button" id="button-addon2"><i class="bi bi-search"></i> </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
         <?php endif ?>
-    </header>
+
     <main id="main" class="flex-grow-1">
         <div class="container-fluid">
         <?php if ($this->section('main')): ?>
